66e2a8ba4e635e1bdf3373d794bb9edffaedd567
jchap12286 <108884746+jchap12286@users.noreply.github.com>
2022-09-19 22:07:30 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+var myString = ""
+
+function loadGame(output) {
+
+    console.log("loadGame was called")
+  
+    console.log(output);
+    
+  
+  }
+
+document.getElementById("restoreButton").addEventListener("click", function() {
+    console.log(myString);
+  });
+
+//   var myString = document.getElementById("result").innerHTML;
+//   console.log(myString);
+
